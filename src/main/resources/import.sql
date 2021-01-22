99e8ce36665e4727fd455e40236d3382127c7efd
zeluzel <zeluzel@gmail.com>
2021-01-22 16:42:06 +0100

    initialization of MySQL DB (production profile)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO institutions(name, description)
+VALUES ('Dbam o Zdrowie', 'Pomoc dzieciom z ubogich rodzin'),
+       ('A kogo', 'Pomoc wybudzaniu dzieci ze śpiączki'),
+       ('Dla dzieci', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej'),
+       ('Bez domu', 'Pomoc dla osób nie posiadających miejsca zamieszkania');
