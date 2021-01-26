096cc262d1702d2fc05f30473e42caea5555e8c6
zeluzel <zeluzel@gmail.com>
2021-01-26 19:49:19 +0100

    #2 :: render list of institutions on landing page

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,6 @@ INSERT INTO institutions(name, description)
 VALUES ('Dbam o Zdrowie', 'Pomoc dzieciom z ubogich rodzin'),
        ('A kogo', 'Pomoc wybudzaniu dzieci ze śpiączki'),
        ('Dla dzieci', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej'),
-       ('Bez domu', 'Pomoc dla osób nie posiadających miejsca zamieszkania');
+       ('Bez domu', 'Pomoc dla osób nie posiadających miejsca zamieszkania'),
+       ('Test', 'Sprawdźmy, czy poprawnie wyświetli się nieparzysta liczba instytucji');
+#        ('Test 2', 'A jak ze zmieszczeniem się tej?');
